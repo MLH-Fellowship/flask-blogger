a0426e1c9597da4348acb21292b5d3c4b0452a65
Sebastian Crossa <sebastiancrossa@hotmail.com>
2021-06-09 10:36:23 -0500

    feat: base html + about page

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
