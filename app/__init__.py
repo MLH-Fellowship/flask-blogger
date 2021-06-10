790c939f4e37f523626b2febfb81b0459db82d2b
Sebastian Crossa <sebastiancrossa@users.noreply.github.com>
2021-06-10 16:37:36 -0500

    feat: About + portfolio routes (#1)

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,25 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/about/ishika')
+def about_ishika():
+    return render_template('ishika.html', fellowName='Ishika');
+
+@app.route('/about/frankie')
+def about_frankie():
+    return render_template('frankie.html', fellowName='Frankie');
+
+@app.route('/about/sebastian')
+def about_sebastian():
+    return render_template('sebastian.html', fellowName='Sebastian');
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
 @app.route('/post')
