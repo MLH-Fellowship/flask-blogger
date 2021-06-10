0124b7a43fc1d5605567227736aecdbe3e85e9b1
Sebastian Crossa <sebastiancrossa@hotmail.com>
2021-06-10 10:41:02 -0500

    feat: add nav + theme style color

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('base.html')
 
 @app.route('/about')
 def about():
