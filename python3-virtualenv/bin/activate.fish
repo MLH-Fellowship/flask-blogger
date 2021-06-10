1c3845a70fe7c61814067174c6bd69b1de3e9df5
Sebastian Crossa <sebastiancrossa@hotmail.com>
2021-06-10 16:10:49 -0500

    chore: add the rest of sections from the resume part

--- python3-virtualenv/bin/activate.fish
+++ python3-virtualenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/frankiesmac/Documents/MLH/flask-blogger/python3-virtualenv"
+set -gx VIRTUAL_ENV "/Users/crossa/dev/flask-blogger/python3-virtualenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
